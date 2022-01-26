1b00cdb53bdfc0805efda558d9db46d43f8b8b68
saroj2052 <sarojshah2020@gmail.com>
2022-01-26 18:07:45 +0545

    yes

--- role/output.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip" {
-    value = aws_instance.ec2.public_ip
-  
-}
--- role/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "sarojshah" {
-  bucket = "sarojshah"
-  acl    = "private"
-
-  tags = {
-    Name        = "saroj-bucket"
-  }
-}
